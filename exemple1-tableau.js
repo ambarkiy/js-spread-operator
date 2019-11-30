a9a860af9d6acd528c5f36c0fbea67cfc9d09e01
ambarkiy <ambarki.youssef@gmail.com>
2019-11-30 06:57:05 +0100

    syntaxe de décomposition pour les tableaux

---
+++ exemple1-tableau.js
@@ -0,0 +1,8 @@
+var langages = ['C#', 'T-SQL', 'Javascript', 'Angular'];
+var newLangage = 'React js';
+var oldLangage = 'Cobol';
+
+var cvSearchKeyWords = [oldLangage, ...langages, newLangage]
+
+console.log(cvSearchKeyWords);
+
