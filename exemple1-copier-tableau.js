490688580724106465a83f461ae8cdad30488e9c
ambarkiy <ambarki.youssef@gmail.com>
2019-11-30 08:04:29 +0100

    renommage

--- exemple1-copier-tableau.js
+++ exemple1-copier-tableau.js
@@ -2,4 +2,5 @@
 
 var draftVersions = ['v1','v2','v3'];
 var newDraftVersions = [...draftVersions] ;
+
 console.log(newDraftVersions); //[ 'v1', 'v2', 'v3' ]
--- exemple1-tableau.js
+++
@@ -1,7 +0,0 @@
-var langages = ['C#', 'T-SQL',];
-var newLangage = 'Javascript';
-var oldLangage = 'Cobol';
-
-var cvSearchKeyWords = [oldLangage, ...langages, newLangage]
-
-console.log(cvSearchKeyWords); // [ 'Cobol', 'C#', 'T-SQL', 'Javascript' ]
